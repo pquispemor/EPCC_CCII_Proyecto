997e5818c5cbd56bbf9443aa4eeee0ef266aa3ef
Diego Alonso Zanabria Sacsi <83051094+Diegodaz@users.noreply.github.com>
2022-08-21 23:49:28 -0500

    Update MainMenuExtras.h

--- InvasionCovid/MainMenuExtras.h
+++ InvasionCovid/MainMenuExtras.h
@@ -1,20 +1,29 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <iostream>
+//Esta clse nos permite poner la opciones Jugar-Controles-Creditos-Salir
 class MainMenuExtras : public sf::Drawable
 {
 private:
+    //Guarda lo que se seleccionada
     int MainMenuSeleccion;
+     //Fuente de tipo de letra
     sf::Font font;
+    //Array de tamaño 4
     sf::Text mainMenu[4];
-
+    //Iniciamos todas las variables
     void iniciarExtras();
 public:
+    //constructor y destructor
     MainMenuExtras();
     ~MainMenuExtras();
+    //Metodo Mover Arriba
     void MoverArriba();
+    //Metodo Mover Abajo
     void MoverAbajo();
+    //Guarda la seleccion
     int MainMenuPressed();
+    //recibe (ventana-estado) y es constante
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
 };
