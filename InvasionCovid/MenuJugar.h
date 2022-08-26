d550fc1a45f0be38a01294abf08d8ee52169bb88
Diego Alonso Zanabria Sacsi <83051094+Diegodaz@users.noreply.github.com>
2022-08-25 23:25:54 -0500

    Update MenuJugar.h

--- InvasionCovid/MenuJugar.h
+++ InvasionCovid/MenuJugar.h
@@ -6,7 +6,9 @@
 #include <SFML/System.hpp>
 #include <iostream>
 #include "Menu.h"
-
+/*Dentro del MenúJugar solo encontraremos métodos como cargar un sprite (.png) asi
+como también cargar string (opciones 1 Jugador y 2 Jugadores) y por último el método
+draw que sirve para que esta clase pueda ser dibujada.*/
 class MenuJugar : public Menu
 {
 private:
