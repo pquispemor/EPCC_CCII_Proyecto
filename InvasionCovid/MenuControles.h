e29ddc7b36f2c8fed540d4d1eaf0a34027ef7cb0
Diego Alonso Zanabria Sacsi <83051094+Diegodaz@users.noreply.github.com>
2022-08-25 23:35:37 -0500

    Update MenuControles.h

--- InvasionCovid/MenuControles.h
+++ InvasionCovid/MenuControles.h
@@ -5,6 +5,9 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include "Menu.h"
+
+/*Esta clase solo se encarga de cargar un sprite (.png) y también cuenta el método draw
+para hacer que esta clase sea dibujable.*/
 class MenuControles : public Menu
 {
 private:
